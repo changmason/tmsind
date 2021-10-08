94280fb4eb108f5b4fdc96d51613a303b30538ae
Mason Chang <changmason@gmail.com>
2021-10-08 16:52:16 +0800

    Remove unnecessary validation for company email

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -5,11 +5,5 @@ RSpec.describe Company, type: :model do
 
   context 'validations' do
     it { should validate_presence_of(:name).on(:update) }
-
-    it 'should validate format of email' do
-      company.email = 'invalid@email_address'
-      expect(company).not_to be_valid
-      expect(company.errors[:email]).to eq(['is invalid'])
-    end
   end
 end
